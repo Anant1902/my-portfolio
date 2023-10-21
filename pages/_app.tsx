297aa25c6be76da4c0c69d7d41fde04e3f18a6dc
Anant Shanker <anant@MacBook-Air-51.local>
2023-10-22 05:26:44 +0800

    fix: Fixed chakraui init issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+            <ColorModeScript initialColorMode="dark"></ColorModeScript>
+            <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,9 @@ import MyApp from './components/MyApp';
 
 export default function Home() {
   return (
-      <ChakraProvider>
-          <ColorModeScript initialColorMode="dark"></ColorModeScript>
+      // <ChakraProvider>
+      //     <ColorModeScript initialColorMode="dark"></ColorModeScript>
           <MyApp />
-      </ChakraProvider>
+      // </ChakraProvider>
   )
 }
